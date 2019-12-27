f7a3b51d41a6b9852a8cb0d1b59633e443c5f073
Dmitry Zhbankov <dmitry.zhbankov@gmail.com>
2019-12-27 09:00:30 +0300

    Update ArticleController

--- ShareMe/Areas/Admin/Controllers/ArticleController.cs
+++ ShareMe/Areas/Admin/Controllers/ArticleController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ShareMe.DAL.UnitOfWork;
 using ShareMe.Models.Entity;
+using ShareMe.Models.ViewModel;
 
 namespace ShareMe.Areas.Admin.Controllers
 {
@@ -30,44 +31,74 @@ namespace ShareMe.Areas.Admin.Controllers
             return View(articles);
         }
 
-        public IActionResult Edit(int? articleId)
-        {
-            if (articleId == null)
-            {
-                return NotFound();
-            }
+        //public IActionResult Edit(int? articleId)
+        //{
+        //    if (articleId == null)
+        //    {
+        //        return NotFound();
+        //    }
 
+        //    var article = unitOfWork.ArticleRepository.GetById(articleId);
+        //    return View("Update", article);
+        //}
+
+        public IActionResult Update(int? authorId, int? categoryId, int? articleId)
+        {
             var article = unitOfWork.ArticleRepository.GetById(articleId);
-            return View("Update",article);
-        }
 
-        public IActionResult Update()
-        {
-            return View();
+            var authors = authorId == null
+                ? unitOfWork.AuthorRepository.Get(x => true)
+                : unitOfWork.AuthorRepository.Get(x => x.AuthorId == authorId);
+            var categories = categoryId == null
+                ? unitOfWork.CategoryRepository.Get(x => true)
+                : unitOfWork.CategoryRepository.Get(x => x.CategoryId == categoryId);
+
+            var tags = unitOfWork.TagRepository.Get(x => true).Select(x => new TagViewModel()
+            {
+                Tag = x,
+                IsChecked = x.ArticleTags.Any(y => y.ArticleId == article?.ArticleId)
+            });
+
+            var articleViewModel = new ArticleViewModel()
+            {
+                Article = article,
+                Authors = authors,
+                Categories = categories,
+                Tags = tags
+            };
+
+            return View(articleViewModel);
         }
 
         [HttpPost]
-        public ActionResult Update(Article article, IFormFile preview)
+        public ActionResult Update(ArticleViewModel articleViewModel, IFormFile preview)
         {
-            if (article == null)
+            if (articleViewModel == null)
             {
                 return BadRequest();
             }
 
-            var articleToUpdate = unitOfWork.ArticleRepository.GetById(article.ArticleId);
+            var articleToUpdate = unitOfWork.ArticleRepository.GetById(articleViewModel.Article.ArticleId);
 
-            if (articleToUpdate==null)
+            if (articleToUpdate == null)
             {
-                article.Date = DateTime.Now;
+                articleViewModel.Article.Date = DateTime.Now;
+
+                var author = unitOfWork.AuthorRepository.GetById(articleViewModel.Author.AuthorId);
+                var category = unitOfWork.CategoryRepository.GetById(articleViewModel.Category.CategoryId);
+
+                articleToUpdate = new Article()
+                {
+                    Author = author,
+                    Category = category
+                };
             }
-            else
-            {
-                articleToUpdate.Content = article.Content;
-                articleToUpdate.Annotation = article.Annotation;
-                MemoryStream ms=new MemoryStream();
-                preview.CopyTo(ms);
-                articleToUpdate.Preview = ms.ToArray();
-            }
+
+            articleToUpdate.Content = articleViewModel.Article.Content;
+            articleToUpdate.Annotation = articleViewModel.Article.Annotation;
+            var ms = new MemoryStream();
+            preview.CopyTo(ms);
+            articleToUpdate.Preview = ms.ToArray();
 
             try
             {
---
+++ ShareMe/Models/ViewModel/TagViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShareMe.Models.Entity;
+
+namespace ShareMe.Models.ViewModel
+{
+    public class TagViewModel
+    {
+        public Tag Tag { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
